b6a633b575bf02e6fbbff4fb2963bbbeb52e5e32
Raphael Vidal <raphael.r.vidal@gmail.com>
2024-09-05 08:05:45 -0300

    Ajustes de comentários

--- www/src/Observer/Interfaces/Observador.php
+++ www/src/Observer/Interfaces/Observador.php
@@ -4,8 +4,9 @@ namespace RaphaelVidal\DesignPatterns\Observer\Interfaces;
 
 /*
     Interface do Observador
-    Define o método statusUpdate, que é chamado para atualizar os observadores
-    quando o estado do sujeito muda.
+    
+    Define o método statusUpdate, que é chamado para 
+    atualizar os observadores quando o estado do sujeito muda.
 */
 interface Observador {
     public function statusUpdate($pedido);
--- www/src/Observer/Observadores/RelatorioGerador.php
+++ www/src/Observer/Observadores/RelatorioGerador.php
@@ -11,6 +11,7 @@ use RaphaelVidal\DesignPatterns\Observer\Interfaces\Observador;
 class RelatorioGerador implements Observador {
     public function statusUpdate($pedido) {
         // Gera um relatório com o novo status do pedido
-        echo "Relatório: O pedido " . $pedido->getId() . " agora está com status: " . $pedido->getStatus() . ".\n";
+        echo "Relatório: O pedido " . $pedido->getId() . "
+        agora está com status: " . $pedido->getStatus() . ".\n";
     }
 }
